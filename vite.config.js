4977131cbfc668cdca42e9312b2853f5f3833dab
Monika-koduje <69014445+Monika-koduje@users.noreply.github.com>
2024-11-02 20:56:16 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
   plugins: [plugin()],
-  base: "/Shop_GOOD_Clothes//",
+  base: "/vite-react-deploy/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
