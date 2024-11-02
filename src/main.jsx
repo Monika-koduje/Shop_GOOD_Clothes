c0aa70120602d0f05aa656d77e46abb93f40f1a7
Monika-koduje <69014445+Monika-koduje@users.noreply.github.com>
2024-11-02 21:23:43 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -22,7 +22,7 @@ import { ForgotPassword } from './pages/ForgotPassword.jsx';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <BrowserRouter basename={import.meta.env.BASE_URL}>
+        <BrowserRouter basename="/Shop_GOOD_Clothes/">
             <Routes>
                 <Route path='/' element={<Layout />}>
                     <Route index element={<App />} />
