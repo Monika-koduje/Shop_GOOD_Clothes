dcc902223b6b2a9c30197584dfba62e8c28a9305
Monika-koduje <m.osenkowska1@gmail.com>
2026-04-09 22:05:28 +0200

    change in User

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -232,7 +232,7 @@ export const User = () => {
                                 <td>{order.quantity}</td>
                                 <td>{order.category}</td>
                                 <td>
-                                    <Image thumbnail className="img-carousel img-size" src={`/Shop_GOOD_Clothes/Shop_GOOD_Clothes/image/${order.imageUrl}`} />
+                                    <Image thumbnail className="img-carousel img-size" src={`/Shop_GOOD_Clothes/Shop_GOOD_Clothes${order.imageUrl}`} />
                                 </td>
                                 <td>{order.price}</td>
                             </tr>
